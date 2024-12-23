104aa52d4b7af5a878c43954ee4d311dc95ba04b
Tatiana Konischeva <moyzaldi@gmail.com>
2024-12-23 11:49:21 +0300

    Форматирование

--- utils/attach.py
+++ utils/attach.py
@@ -6,16 +6,13 @@ def add_screenshot(browser):
     png = browser.driver.get_screenshot_as_png()
     allure.attach(body=png, name='screenshot', attachment_type=AttachmentType.PNG, extension='.png')
 
-
 def add_logs(browser):
     log = "".join(f'{text}\n' for text in browser.driver.get_log(log_type='browser'))
     allure.attach(log, 'browser_logs', AttachmentType.TEXT, '.log')
 
-
 def add_html(browser):
     html = browser.driver.page_source
     allure.attach(html, 'page_source', AttachmentType.HTML, '.html')
-
 
 def add_video(browser):
     video_url = "https://selenoid.autotests.cloud/video/" + browser.driver.session_id + ".mp4"
