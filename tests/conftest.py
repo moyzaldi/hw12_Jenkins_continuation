c5cbe4c5e4abe10d9376e4002fe12c985df0f98a
Tatiana Konischeva <moyzaldi@gmail.com>
2024-12-23 09:59:04 +0300

    Добавлена параметризация

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,36 @@
 import allure
 import pytest
+import os
+
 from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+from dotenv import load_dotenv
 from utils import attach
 
 
-@pytest.fixture(scope='function', autouse=True)
-def browser_settings():
+DEFAULT_BROWSER_VERSION = '126.0'
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='126.0'
+    )
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
+
+
+@pytest.fixture(scope='function')
+def browser_settings(request):
     with allure.step("Параметры браузера"):
+        browser_version = request.config.getoption('--browser_version')
+        browser_version = browser_version if browser_version != '' else DEFAULT_BROWSER_VERSION
         options = Options()
         selenoid_capabilities = {
             "browserName": "chrome",
-            "browserVersion": "126.0",
+            "browserVersion": browser_version,
             "selenoid:options": {
                 "enableVNC": True,
                 "enableVideo": True
@@ -21,15 +38,20 @@ def browser_settings():
         }
 
         options.capabilities.update(selenoid_capabilities)
+        login = os.getenv('LOGIN')
+        password = os.getenv('PASSWORD')
+        selenoid_host = os.getenv('SELENOID_HOST')
+
+
         driver = webdriver.Remote(
-            command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+            command_executor=f"https://{login}:{password}@{selenoid_host}/wd/hub",
             options=options)
 
         browser.config.driver = driver
-        browser.config.window_height = 1080
-        browser.config.window_width = 1920
+
         browser.config.base_url = 'https://demoqa.com'
-        # browser.config.timeout = 20
+        options.page_load_strategy = 'eager'
+        browser.driver.fullscreen_window()
 
         yield
 
